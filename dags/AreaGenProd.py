47ca2ab34d82b4c9fb4253490c08b898c83b312d
bramvanslooten <bramvanslooten@gmail.com>
2018-06-03 19:14:35 +0200

    dags update1

--- dags/AreaGenProd.py
+++ dags/AreaGenProd.py
@@ -7,7 +7,7 @@ default_args = {
 	'depends_on_past': False,
 	'retries': 2,
 	'retry_delay': timedelta(minutes=5),
-	'start_date': datetime(2017,1,1),
+	'start_date': datetime(2017, 1, 1),
 	'execution_timeout': timedelta(hours=1),
 	'email_on_failure': False,
 	'email_on_retry': False,
@@ -15,14 +15,16 @@ default_args = {
 
 
 dag = DAG(
-	'Areagen_Elexon', default_args=default_args, schedule_interval(1))
+	'Elexon_Areagen_actual', default_args=default_args, schedule_interval(1))
 
 t1 = bashOperator(
-	task_id = 'scraper',
-	bash_command='',
+	task_id='scraper',
+	bash_command='/home/bram/testetl/scrapy/scrapy crawl AreaGen -a STARTDATE=%(ds)s',
 	dag=dag)
 
 t2 = bashOperator(
-	task_id = 'dumper',
-	bash_command='',
+	task_id='dumper',
+	bash_command='/home/bram/testetl/Main/python AreaGen_actual %(ds)s',
 	dag=dag)
+
+t1 >> t2
