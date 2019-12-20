a107e74792861fdb65a4419bc64caf5aebf61f0a
bram <bramvanslooten@gmail.com>
2019-12-20 17:03:08 +0100

    formatting

--- dags/AreaConProd.py
+++ dags/AreaConProd.py
@@ -3,27 +3,27 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-	'owner': 'bram',
-	'depends_on_past': False,
-	'retries': 2,
-	'retry_delay': timedelta(minutes=5),
-	'start_date': datetime(2017, 1, 1),
-	'execution_timeout': timedelta(hours=1),
-	'email_on_failure': False,
-	'email_on_retry': False,
+    'owner': 'bram',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2017, 1, 1),
+    'execution_timeout': timedelta(hours=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
 }
 
 dag = DAG(
-	'Elexon_Areacon_actual', default_args=default_args, schedule_interval=timedelta(1))
+    'Elexon_Areacon_actual', default_args=default_args, schedule_interval=timedelta(1))
 
 t1 = BashOperator(
-	task_id='scraper',
-	bash_command='cd /home/bram/testetl/scrapy && scrapy crawl AreaCon -a STARTDATE={{ ds }}',
-	dag=dag)
+    task_id='scraper',
+    bash_command='cd /home/bram/testetl/scrapy && scrapy crawl AreaCon -a STARTDATE={{ ds }}',
+    dag=dag)
 
 t2 = BashOperator(
-	task_id='dumper',
-	bash_command='cd /home/bram/testetl/Main && python AreaCon_actual.py {{ ds }}',
-	dag=dag)
+    task_id='dumper',
+    bash_command='cd /home/bram/testetl/Main && python AreaCon_actual.py {{ ds }}',
+    dag=dag)
 
 t1 >> t2
--- dags/Elexon_Area_forecastCon.py
+++ dags/Elexon_Area_forecastCon.py
@@ -3,28 +3,28 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-	'owner': 'bram',
-	'depends_on_past': False,
-	'retries': 2,
-	'retry_delay': timedelta(minutes=5),
-	'start_date': datetime(2017, 1, 1),
-	'execution_timeout': timedelta(hours=1),
-	'email_on_failure': False,
-	'email_on_retry': False,
+    'owner': 'bram',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2017, 1, 1),
+    'execution_timeout': timedelta(hours=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
 }
 
 dag = DAG(
-	'Elexon_Area_forecastCon', default_args=default_args,
-	schedule_interval='@hourly', catchup=False)
+    'Elexon_Area_forecastCon', default_args=default_args,
+    schedule_interval='@hourly', catchup=False)
 
 t1 = BashOperator(
-	task_id='scraper',
-	bash_command='cd /home/bram/testetl/scrapy && scrapy crawl Elexon_AreaCon_Forecast -a STARTDATE={{ ds }}',
-	dag=dag)
+    task_id='scraper',
+    bash_command='cd /home/bram/testetl/scrapy && scrapy crawl Elexon_AreaCon_Forecast -a STARTDATE={{ ds }}',
+    dag=dag)
 
 t2 = BashOperator(
-	task_id='dumper',
-	bash_command='cd /home/bram/testetl/Main && python Elexon_AreaCon_Forecast.py {{ ds }}',
-	dag=dag)
+    task_id='dumper',
+    bash_command='cd /home/bram/testetl/Main && python Elexon_AreaCon_Forecast.py {{ ds }}',
+    dag=dag)
 
 t1 >> t2
