832fd8cf890ffe56c2e2adb934ac5ce573150045
Bram van Slooten <bvs@priogen.nl>
2020-01-02 14:55:45 +0100

    timezone

--- Main/AreaGen_actual.py
+++ Main/AreaGen_actual.py
@@ -34,7 +34,7 @@ dumper = SQL.common(conn, cur, dumper, 'source')
 
 dumper = SQL.Elexontime(dumper)
 
-tze = timezone('Europe/Amsterdam')
+tze = timezone('UTC')
 dumper['dump_date'] = pd.to_datetime('now')
 dumper['dump_date'] = dumper.dump_date.dt.tz_localize(tze)
 dumper['period'] = pd.Timedelta('30 minutes')
