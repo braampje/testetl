a215b6666f99f5c8a4916ee451398d8da49c57f0
bram <bramvanslooten@gmail.com>
2020-02-11 13:15:20 +0100

    derived

---
+++ scrapy/Elexon/spiders/Elexon_derived.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import XMLFeedSpider
+from Elexon.items import derived
+import os
+from datetime import date, timedelta
+from dateutil.parser import parse
+
+
+class derived_spider(XMLFeedSpider):
+
+    name = "Elexon_derived"
+    allowed_domains = 'bmreports.com'
+    itertag = 'item'
+    iterator = 'xml'
+    download_timeout = 600
+
+#   delimiter = ','
+
+#   headers = [
+#       'Record Type', 'BMunitid', 'Date', 'value1', 'value2', 'value3', 'value4', 'value5'
+#   ]
+
+    custom_settings = {
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': '../Main/csv/Elexon_derived_%(STARTDATE)s.csv'
+    }
+
+    def start_requests(self):
+
+        VERSION_NUMBER = 'v1'
+        API_Key = '9urjhfmw814sqhn'
+        TYPE = 'DERBMDATA'
+        SERVICETYPE = 'XML'
+
+        url = 'https://api.bmreports.com/BMRS/' + TYPE + \
+            '/' + VERSION_NUMBER + '?APIKey=' + API_Key
+        url = url + '&ServiceType=' + SERVICETYPE + '&SettlementDate=' + \
+            self.STARTDATE
+
+        yield scrapy.Request(url, self.parse)
+
+    def __init__(self, *args, **kwargs):
+
+        STARTD = kwargs.pop('STARTDATE', date.today().isoformat())
+        self.STARTDATE = STARTD
+
+        if os.path.exists('../Main/csv/Elexon_derived_' + self.STARTDATE + '.csv'):
+            os.remove('../Main/csv/Elexon_derived_' +
+                      self.STARTDATE + '.csv')
+
+        super(bsad_spider, self).__init__(*args, **kwargs)
+
+    def parse_node(self, response, node):
+        item = derived()
+        item['Date'] = node.xpath('settDate/text()').extract()
+        item['Period'] = node.xpath('settlementPeriod/text()').extract()
+        item['cost'] = node.xpath('cost/text()').extract()
+        item['volume'] = node.xpath('volume/text()').extract()
+        item['action_id'] = node.xpath('id/text()').extract()
+        item['active_flag'] = node.xpath('activeFlag/text()').extract()
+        item['so_flag'] = node.xpath('soFlag/text()').extract()
+        item['stor_flag'] = node.xpath('bsaaSTORProviderFlag/text()').extract()
+        #print(True if not node.xpath('qpnLevelFrom/text()').extract() else False)
+        return item
